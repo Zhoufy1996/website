ecf65126f970a945228869e275ed8455c942a7a0
zhoufeiyu <zhoufeiyu@lianfan.com>
2022-03-16 17:31:40 +0800

    暂存

--- src/components/epic7/SimulateEnhance.tsx
+++ src/components/epic7/SimulateEnhance.tsx
@@ -1,13 +1,16 @@
-import { Box, Button } from '@mui/material';
+import { Box, MenuItem } from '@mui/material';
 import {
   useEffect, useMemo, useRef, useState,
 } from 'react';
 import { useRecoilValue } from 'recoil';
+import LoadingButton from '@mui/lab/LoadingButton';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { equipmentErrorsState, equipmentState, personTemplateState } from '../../store/epic7/equipment';
-import { Equipment } from '../../types/epic7';
+import { Equipment, EquipmentAttributeCode } from '../../types/epic7';
 import { calcEqipmentScore, enhanceMax } from '../../utils/epic7';
 import CustomTextField from './CustomTextField';
 import EquipmentEnhancedChart from './ScoreChart';
+import { equipmentAttributeOptions } from '../../data/epic7';
 
 interface ChartDataItem {
   score: number;
@@ -22,23 +25,29 @@ const SimulateEnhance = () => {
 
   const [enhancedData, setEnhancedData] = useState<Equipment[]>([]);
 
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const [chartDataType, setChartDataType] = useState <EquipmentAttributeCode | 'score'>('score');
+
   const workerRef = useRef<Worker>();
   useEffect(() => {
-    workerRef.current = new Worker('./test.worker.js');
-    workerRef.current.onmessage = (e) => {
-      setEnhancedData(e.data);
-    };
-    workerRef.current.onerror = (e) => {
-      console.log(e);
-    };
-    console.log(workerRef);
-    return () => {
-      workerRef.current?.terminate();
-    };
+    if (window.Worker) {
+      workerRef.current = new Worker(new URL('./enhance.worker.ts', import.meta.url));
+      workerRef.current.addEventListener('message', (e) => {
+        setEnhancedData(e.data);
+        setLoading(false);
+      });
+
+      return () => {
+        workerRef.current?.terminate();
+      };
+    }
+    return () => {};
   }, []);
 
   const enhance = () => {
-    if (Number(simulateCount) > 10000) {
+    setLoading(true);
+    if (workerRef.current && Number(simulateCount) > 10000) {
       workerRef.current?.postMessage({
         equipment: equipmentValue,
         count: simulateCount,
@@ -47,38 +56,73 @@ const SimulateEnhance = () => {
       setEnhancedData(new Array(Number(simulateCount)).fill(1).map(() => {
         return enhanceMax(equipmentValue);
       }));
+      setLoading(false);
     }
   };
 
   const chartData = useMemo(() => {
     const scoreMap = new Map<number, number>();
-    enhancedData.map((item) => {
-      return calcEqipmentScore({
-        equipmentProperty: item,
-        personTemplate,
-      });
-    }).forEach((score) => {
-      if (scoreMap.get(score) == null) {
-        scoreMap.set(score, 1);
-      } else {
-        scoreMap.set(score, scoreMap.get(score) as number + 1);
-      }
-    });
-
     const result: ChartDataItem[] = [];
+    if (chartDataType === 'score') {
+      enhancedData.map((item) => {
+        return calcEqipmentScore({
+          equipmentProperty: item,
+          personTemplate,
+        });
+      }).forEach((score) => {
+        if (scoreMap.get(score) == null) {
+          scoreMap.set(score, 1);
+        } else {
+          scoreMap.set(score, scoreMap.get(score) as number + 1);
+        }
+      });
+    } else {
+      enhancedData.map((equipment) => {
+        return equipment.subAttributes.find((attribute) => attribute.code === chartDataType)?.value
+         || 0;
+      }).forEach((score) => {
+        if (scoreMap.get(score) == null) {
+          scoreMap.set(score, 1);
+        } else {
+          scoreMap.set(score, scoreMap.get(score) as number + 1);
+        }
+      });
+    }
     scoreMap.forEach((count, score) => {
       result.push({
         count,
         score,
       });
     });
-
     return result;
-  }, [enhancedData, personTemplate]);
+  }, [enhancedData, personTemplate, chartDataType]);
 
   return (
     <div>
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+        <CustomTextField
+          id="chartDataType"
+          label="图像x轴"
+          type="select"
+          select
+          value={chartDataType}
+          onChange={(e) => {
+            setChartDataType(e.target.value as EquipmentAttributeCode | 'score');
+          }}
+        >
+          <MenuItem value="score">分数</MenuItem>
+          {
+            Object.entries(equipmentAttributeOptions).sort((left, right) => {
+              return left[1].sortNo - right[1].sortNo;
+            }).map(([code, options]) => {
+              return (
+                <MenuItem value={code} key={code}>
+                  {options.label}
+                </MenuItem>
+              );
+            })
+          }
+        </CustomTextField>
         <CustomTextField
           id="simulateCount"
           label="强化次数"
@@ -91,8 +135,23 @@ const SimulateEnhance = () => {
           onChange={(e) => {
             setSimulateCount(e.target.value);
           }}
+          sx={{
+            mr: 1,
+          }}
         />
-        <Button onClick={enhance} disabled={equipmentErrors.length > 0}>强化</Button>
+        <LoadingButton
+          loading={loading}
+          onClick={enhance}
+          disabled={equipmentErrors.length > 0}
+          variant="contained"
+          loadingPosition="end"
+          endIcon={(
+            <ArrowForwardIcon />
+          )}
+        >
+          强化
+
+        </LoadingButton>
       </Box>
       {
         chartData.length > 0 && (
---
+++ src/components/epic7/enhance.worker.ts
@@ -0,0 +1,14 @@
+import { enhanceMax } from '../../utils/epic7';
+
+declare let self: Worker;
+
+const ctx: Worker = self as unknown as Worker;
+
+ctx.addEventListener('message', (evt) => {
+  const { equipment, count } = (evt as MessageEvent<any>).data;
+  const reuslt = new Array(Number(count)).fill(1).map(() => {
+    return enhanceMax(equipment);
+  });
+
+  postMessage(reuslt);
+});
