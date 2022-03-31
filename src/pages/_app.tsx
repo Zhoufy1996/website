f233208632f0d3fef87f5b2f69532266817fb5ef
zhoufeiyu <zhoufeiyu@lianfan.com>
2022-03-31 13:46:51 +0800

    修复 pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Epic7 Tools</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <ThemeProvider theme={theme}>
         <CssBaseline />
